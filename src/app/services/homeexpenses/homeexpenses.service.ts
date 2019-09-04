9030e32d0f444b490e551da9fdb54c07d47cdaf1
ponnalaxmikanth <laxmikanth.ponna@hotmail.com>
2019-09-03 21:45:59 -0400

    - ui changes

--- src/app/services/homeexpenses/homeexpenses.service.ts
+++ src/app/services/homeexpenses/homeexpenses.service.ts
@@ -25,7 +25,7 @@ export class HomeexpensesService {
 
     this.getAccountTypes().subscribe(val => {
       this.accountTypes = val;
-      //console.log('HomeexpensesService -- getAccountTypes', this.accountTypes);
+      // console.log('HomeexpensesService -- getAccountTypes', this.accountTypes);
     },
     error => {
       console.error('HomeexpensesService -- getAccountTypes', error);
@@ -33,7 +33,7 @@ export class HomeexpensesService {
 
     this.getAccountDetails().subscribe(val => {
       this.acounts = val;
-      //console.log('HomeexpensesService -- getAccountDetails', this.acounts);
+      // console.log('HomeexpensesService -- getAccountDetails', this.acounts);
     },
     error => {
       console.error('HomeexpensesService -- getAccountDetails', error);
@@ -41,7 +41,7 @@ export class HomeexpensesService {
 
     this.getExpenseGroups().subscribe(val => {
       this.expenseGroups = val;
-      //console.log('HomeexpensesService -- getExpenseGroups', this.expenseGroups);
+      // console.log('HomeexpensesService -- getExpenseGroups', this.expenseGroups);
     },
     error => {
       console.error('HomeexpensesService -- getExpenseGroups', error);
@@ -49,7 +49,7 @@ export class HomeexpensesService {
 
     this.getExpenseSubGroups().subscribe(val => {
       this.expenseSubGroups = val;
-      //console.log('HomeexpensesService -- GetExpenseSubGroups', this.expenseSubGroups);
+      // console.log('HomeexpensesService -- GetExpenseSubGroups', this.expenseSubGroups);
     },
       error => {
         console.error('HomeexpensesService -- GetExpenseSubGroups', error);
@@ -85,19 +85,23 @@ export class HomeexpensesService {
   }
 
   getTransactions(fromDate, toDate, accountId: number): Observable<any> {
-    let request = { FromDate: fromDate, ToDate: toDate, AccountId: accountId };
+    const request = { FromDate: fromDate, ToDate: toDate, AccountId: accountId };
     return this.httpClient.post(this.baseUrl + 'api/Expenses/GetExpenses', request, httpOptions);
+  }
+
+  getExpenseTransactions(fromDate, toDate): Observable<any> {
+    const request = { FromDate: fromDate, ToDate: toDate };
+    return this.httpClient.post(this.baseUrl + 'api/Expenses/GetBudget', request, httpOptions);
   }
 
   handleError(error: HttpErrorResponse) {
     console.error('HomeexpensesService -- api call error', error);
   }
- getBudgetDetails():Observable<any>{
-   try{
-      return this.httpClient.get("assets/Data/Budget/budget.json");
-   }
-   catch(exc){
-     console.log("home expenses service--getbudgetdetails",exc);
+ getBudgetDetails(): Observable<any> {
+   try {
+      return this.httpClient.get('assets/Data/Budget/budget.json');
+   } catch (exc) {
+     console.log('home expenses service--getbudgetdetails', exc);
    }
  }
 }
